7b63c12ec6735e8fce3fc375aad3d1183a3b107e
Julien Savéan <j.savean@gmail.com>
2021-12-06 11:27:05 +0100

    challenge 12

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,6 +31,36 @@ class ProgramController extends AbstractController
             ['programs' => $programs]
         );
     }
+    
+    /**
+     * The controller for the program add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Program Object
+        $Program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $Program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Program Object
+            $entityManager->persist($Program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to programs list
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+    }
+    
     /**
      * @Route("/{id}/", methods={"GET"},  name="show")
      */
@@ -82,4 +114,6 @@ class ProgramController extends AbstractController
             'episode' => $episode,
         ]);
     }
+
+    
 }
