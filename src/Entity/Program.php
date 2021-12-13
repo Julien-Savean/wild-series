a8c521e1ac5116f0958bb19c41db3faced2bb91e
Julien Savéan <j.savean@gmail.com>
2021-12-13 13:40:16 +0100

    challenge 14

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,10 +6,14 @@ use App\Repository\ProgramRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="ce titre existe déjà")
  */
+
 class Program
 {
     /**
@@ -21,11 +25,19 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotNull
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotNull
+     * @Assert\Regex(
+     *      pattern="[plus belle la vie]i",
+     *      match=false,
+     *      message="On parle de vraies séries ici"
+     * )
      */
     private $synopsis;
 
